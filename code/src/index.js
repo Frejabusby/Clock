0fe8e5c582243582ab1efef49c628b1beb90d9d4
Freja Busby <frejabusby@gmail.com>
2018-04-12 11:10:08 +0200

    New repository
    Problem when deploy so needed to do a new repository

--- code/src/index.js
+++ code/src/index.js
@@ -3,16 +3,62 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date()
+    }
+  }
+
+  componentDidMount() {
+    this.startTimer()
+    this.timeOfTheDay()
+  }
+
+  componentWillUnmount() {
+    this.stopTimer()
+  }
+
+  stopTimer = () => {
+    clearInterval(this.timerID)
+  }
+
+  startTimer = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  timeOfTheDay = () => {
+    const hours = this.state.date.getHours()
+    const container = document.getElementById("container")
+    if (hours < 20 && hours > 7) {
+      container.classList.toggle("day-style")
+    } else {
+      container.classList.toggle("night-style")
+    }
+  }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      <div id="container" className="container">
+        <img className="sun-img" src="./sun.png" alt="sun" />
+        <img className="moon-img" src="./moon.png" alt="moon" />
+        <h1>The time is:</h1>
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
+        <button onClick={this.stopTimer} className="button stop-button">Stop the time</button>
+        <button onClick={this.startTimer} className="button start-button">Start the time</button>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(<Clock />, document.getElementById("root"))
